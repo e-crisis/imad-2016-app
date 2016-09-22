50db1a4814d683ac8a67abff26ac4b58e0406bcc
Preetika Mondal <preetikamon11@gmail.com>
2016-09-22 23:35:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,11 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
 //move the image
 var img = document.getElementById('move');
+var marginLeft = 0;
+function moveRight() {
+    marginLeft = marginLeft + 10; //increment value of marginLeft
+    img.style.marginLeft =  marginLeft + 'px';//set the css to that value
+}
 img.onclick = function () {
-    img.style.marginLeft = '100px';//on click image moves to the right
+    var interval = setInterval(moveRight, 100);//every 100 millisec apply the function moveRight
 };
