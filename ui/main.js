186627319ca24064bf8db91d368a2df8aeb3cb1a
Preetika Mondal <preetikamon11@gmail.com>
2016-09-22 23:36:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,5 +10,5 @@ function moveRight() {
     img.style.marginLeft =  marginLeft + 'px';//set the css to that value
 }
 img.onclick = function () {
-    var interval = setInterval(moveRight, 100);//every 100 millisec apply the function moveRight
+    var interval = setInterval(moveRight, 50);//every 100 millisec apply the function moveRight
 };
