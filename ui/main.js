bdd3e77fe2d164a8c78640cc00eb8d7105538474
Preetika Mondal <preetikamon11@gmail.com>
2016-09-23 21:51:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,26 @@
 //counter code
 var button = document.getElementById('counter');
-var counter = 0;
+
 button.onclick = function () {
-    //make a request to the counter endpoint
+    
+    //create a request object 
+    var request = new XMLHttpRequest();
+    
     //capture the response and store it in a variable
-    //render the variable in the correct span
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            //Take some action
+            if (request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+                
+            }
+        }
+        //not done yet
+    };
+    //make the request to the counter endpoint
+    request.open('GET','http://e-crisis.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
 };
