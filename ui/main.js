2a47f0cd353075ebb1ab12e26f1ba29f4eb2550e
Preetika Mondal <preetikamon11@gmail.com>
2016-09-26 21:16:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -62,4 +62,33 @@ submit.onclick = function () {
 
 //submit comment
 
-
+var submitComment = document.getElementById('submit_comment');
+submitComment.onclick = function () {
+    //create a request object
+    var request = new XMLHttpRequest();
+    //capture the response and store it in a variable
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            //Take some action
+            if (request.status === 200) {
+                //capture a list of names and render it as a list
+               var comments = request.responseText;
+               comments = JSON.parse(comments);
+               var list = '';
+               for (var i=0; i<comments.length; i++) {
+                   list += '<li>' + comments[i] + '</li>';
+                   
+               }
+               var ul = document.getElementById('commentList');
+               ul.innerHTML = list;
+            }
+        }
+    };
+    
+     //make the request
+     var commentInput = document.getElementById('comment');
+     var comment = commentInput.value;
+     request.open('GET','http://e-crisis.imad.hasura-app.io/submit-comment?comment=' + comment, true);
+     request.send(null);
+    
+};
