cc18320c58bcbbef58c76967985deaca8aa01f2f
Preetika Mondal <preetikamon11@gmail.com>
2016-11-27 18:59:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,13 +1,35 @@
 var express = require('express');//importing library: creates the webserver
 var morgan = require('morgan');//importing library: outputs log of our server
 var path = require('path');//importing library
-
+var Pool = require('pg').Pool;
+var config = {
+    user: 'e-crisis',
+    database: 'e-crisis',
+    host: 'http://db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var pool = new Pool(config);
+app.get('/test-db', function (req, res) {
+    //make a select request
+    //return a response with results
+    pool.query('SELECT * FROM test', function (err,result){
+        if (err) {
+            res.status(500).send(err.toString());
+        } else {
+            res.send(JSON.stringify(result));
+        }
+        
+    });
+});
+
 var counter = 0;
 app.get('/counter', function (req, res) {
  counter = counter + 1;
