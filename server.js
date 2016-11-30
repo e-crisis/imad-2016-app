9491fd98925c0b14e525859dae9c8d17e73314d2
Preetika Mondal <preetikamon11@gmail.com>
2016-11-30 22:07:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -188,6 +188,11 @@ app.get('/check-login', function (req, res) {
         res.send('you are not logged in');
     }
 });
+
+app.get('/logout', function (req, res) {
+    delete req.session.auth;
+    res.send('logged out');
+});
     
 var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
