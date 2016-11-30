25f8a2f5c8073c1a80ff28a79b1d4c25a857d2dc
Preetika Mondal <preetikamon11@gmail.com>
2016-11-30 21:21:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,31 +1,6 @@
-//counter code
-var button = document.getElementById('counter');
 
-button.onclick = function () {
-    
-    //create a request object 
-    var request = new XMLHttpRequest();
-    
-    //capture the response and store it in a variable
-    request.onreadystatechange = function () {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            //Take some action
-            if (request.status === 200) {
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-                
-            }
-        }
-        //not done yet
-    };
-    //make the request to the counter endpoint
-    request.open('GET','http://e-crisis.imad.hasura-app.io/counter',true);
-    request.send(null);
-    
-};
 
-//submit name
+//submit username and password to login
 
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
@@ -37,26 +12,26 @@ submit.onclick = function () {
             //Take some action
             if (request.status === 200) {
                 //capture a list of names and render it as a list
-               var names = request.responseText;
-               names = JSON.parse(names);
-               var list = '';
-               for (var i=0; i<names.length; i++) {
-                   list += '<li>' + names[i] + '</li>';
-                   
+               console.log('user logged in');
+               alert('Congrats!!The magic has been UNLOCKED!');
+               } else if (request.status === 403) {
+                   alert('uh-oh! wrong username or password :(');
+               } else if (request.status === 500) {
+                   alert('something went wrong with the magic wand');
                }
-               var ul = document.getElementById('nameList');
-               ul.innerHTML = list;
-                
+               
             }
-        }
         //not done yet
     };
      
      //make the request
-     var nameInput = document.getElementById('name');
-     var name = nameInput.value;
-     request.open('GET','http://e-crisis.imad.hasura-app.io/submit-name?name=' + name, true);
-     request.send(null);
+     var username = document.getElementById('username').value;
+     var password = document.getElementById('password').value;
+     console.log(username);
+     console.log(password);
+     request.open('POST','http://e-crisis.imad.hasura-app.io/login', true);
+     request.setRequestHeader('Content-Type', 'application/json');
+     request.send(JSON.stringify({username: username, password: password}));
    
 };
 
