8f8c49ea9614fd3d66e35da7f301b713ae9b8318
Preetika Mondal <preetikamon11@gmail.com>
2016-12-01 12:46:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -35,6 +35,37 @@ submit.onclick = function () {
    
 };
 
+var register = document.getElementById('register_btn');
+register.onClick = function() {
+    //create a request object
+    var request = new XMLHttpRequest();
+    //capture the response and store it in a variable
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            //Take some action
+            if (request.status === 200) {
+                //capture a list of names and render it as a list
+                alert('User created successfully');
+                register.value = 'Registered!';
+               } else {
+                   alert('Could not register the user');
+                   register.value = 'Register';
+               }
+               
+            }
+        //not done yet
+    };
+    //make the request
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST', '/create-user', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username: username, password: password}));  
+    register.value = 'Registering...';
+
+};
 //submit comment
 
 var submitComment = document.getElementById('submit_comment');
