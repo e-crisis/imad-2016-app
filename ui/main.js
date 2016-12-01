3965c8e7d71b55784edb873ea993d493824dde78
Preetika Mondal <preetikamon11@gmail.com>
2016-12-01 13:06:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -60,7 +60,7 @@ register.onClick = function() {
     var password = document.getElementById('password').value;
     console.log(username);
     console.log(password);
-    request.open('POST', '/create-user', true);
+    request.open('POST', 'http://e-crisis.imad.hasura-app.io/create-user', true);
     request.setRequestHeader('Content-Type', 'application/json');
     request.send(JSON.stringify({username: username, password: password}));  
     register.value = 'Registering...';
